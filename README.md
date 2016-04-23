fffa59e9102217413ec52eff21c54768523c20eb
Paul Liverman III <paul.liverman.iii@gmail.com>
2016-04-22 20:08:36 -0700

    wip shits

---
+++ README.md
@@ -0,0 +1,9 @@
+# GIFS
+
+The Guard13007 Industries File System (GIFS) is a virtual file system for
+ComputerCraft. It emulates the fs API and allows any number of disks to be used
+as one large disk.
+
+More features coming soon(TM).
+
+[1]: http://www.computercraft.info/wiki/Fs_(API)
--- src/gifs.lua
+++ src/gifs.lua
@@ -1,58 +1,33 @@
--- Guard13007 Industries File System
+-- Guard13007 Industries File System (gifs.lua)
+-- Copyright (c) 2016 Paul Liverman, released under MIT license
+-- TODO URL TO REPO
 
-local tree = {}
+local rawfs = fs  -- get our own copy of the fs API
+local fstree = {} -- loaded GIFS file system tree
+local disks = {}  -- loaded physical disk info
 
---TODO determine how to build master tree
+-- disks are numerically indexed, have a mountname and uuid
 
-local function getPath(path)
-    local result = tree
-
-    path = path:gsub("\\", "/")
-    local current = 1
-    local next = path:find("/", current)
-    if current == next then
-        current = 2
+-- checks for disks being added or removed, updates loaded data
+local function checkdisks()
+    local disklist = rawfs.list("/")
+    for _, disk in ipairs(disklist) do
+        for _, loadedDisk in ipairs(disks) do
+            --if disk == loadedDisk.mountname then
+            --    --
+            --end
+        end
     end
-
-    if not next then
-        return nil --NOTE not sure if good idea
-    end
-
-    while next do
-        result = result[path:sub(current, next - 1)]
-        current = next + 1
-        next = path:find("/", current)
-    end
-
-    return result
 end
 
+-- returns reference to node in fstree represented by path
+local function getNode(path)
+    --
+end
+
+-- returns alphabetically ordered numerically indexed list of contents of a path
+-- or an empty table
 function list(path)
-    local result = getPath(path)
-    if type(result) == "table" then
-        --TODO process
-        -- (we can't return it raw, we need to simply get what's in it ??)
-    elseif type(result) == "nil" then
-        error("Not a directory")
-    end
+    checkdisks()
+    local node = getNode(path)
 end
-
---[[
-fs.list(string path)	table files	Returns a list of all the files (including subdirectories but not their contents) contained in a directory, as a numerically indexed table.
-fs.exists(string path)	boolean exists	Checks if a path refers to an existing file or directory.
-fs.isDir(string path)	boolean isDirectory	Checks if a path refers to an existing directory.
-fs.isReadOnly(string path)	boolean readonly	Checks if a path is read-only (i.e. cannot be modified).
-fs.getName(string path)	string name	Gets the final component of a pathname.
-fs.getDrive(string path)	string/nil drive	Gets the storage medium holding a path, or nil if the path does not exist.
-fs.getSize(string path)	number size	Gets the size of a file in bytes.
-fs.getFreeSpace(string path)	number space	Gets the remaining space on the drive containing the given directory.
-fs.makeDir(string path)	nil	Makes a directory.
-fs.move(string fromPath, string toPath)	nil	Moves a file or directory to a new location.
-fs.copy(string fromPath, string toPath)	nil	Copies a file or directory to a new location.
-fs.delete(string path)	nil	Deletes a file or directory.
-fs.combine(string basePath, string localPath)	string path	Combines two path components, returning a path consisting of the local path nested inside the base path.
-fs.open(string path, string mode)	table handle	Opens a file so it can be read or written.
-fs.find(string wildcard)	table files	Searches the computer's files using wildcards. Requires version 1.6 or later.
-fs.getDir(string path)	string parentDirectory	Returns the parent directory of path. Requires version 1.63 or later.
-fs.complete(string partial name, string path [, boolean include files] [, boolean include slashes])	table matches	Returns a list of strings that could be combined with the provided name to produce valid entries in the specified folder. Requires version 1.74 or later.
-]]
